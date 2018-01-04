6ea76f4890dbf77ccf14167081069219ad2421a9
Lukasz Woojcik <lukasz.wojcik@findwise.com>
2018-01-04 14:10:44 -0800

    Init.

---
+++ DatabaseCreator/DatabaseCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseCreator.Properties;
+using Findwise.Sharepoint.SolutionInstaller;
+
+namespace DatabaseCreator
+{
+    public class DatabaseCreator : IInstallerModule
+    {
+        public string Name => throw new NotImplementedException();
+
+        public Image Icon => Resources.if_server_11124;
+
+        public bool IsInstalled => throw new NotImplementedException();
+
+        public void Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Findwise.Sharepoint.SolutionInstaller/IInstallerModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Findwise.Sharepoint.SolutionInstaller
+{
+    public interface IInstallerModule
+    {
+        string Name { get; }
+        Image Icon { get; }
+
+        bool IsInstalled { get; }
+
+
+
+        void Install();
+        void Uninstall();
+    }
+}
