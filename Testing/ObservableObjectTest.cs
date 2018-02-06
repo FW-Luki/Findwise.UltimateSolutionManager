ea5dd15a0ad22bfc6380fd27d559db0d0a5b8da7
Lukasz Woojcik <lukasz.wojcik@findwise.com>
2018-02-06 10:07:06 -0800

    Added test project.

---
+++ Testing/ObservableObjectTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Findwise.Sharepoint.SolutionInstaller.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class ObservableObjectTest
+    {
+        private class TestObject : ObservableObject
+        {
+            public object AnyTypeProperty { get => Property<object>(); set => Property(value); }
+
+            public double MagicNumber { get => Property<double>(); set => Property(value); }
+
+            [DependantOn(nameof(MagicNumber))]
+            public string DependantProperty { get => "Magic number is " + MagicNumber; }
+
+            public ObservableCollection<object> Collection { get => Property(() => new ObservableCollection<object>()); }
+        }
+
+        [TestMethod]
+        public void PropertyOfAnyTypeSetTest()
+        {
+            bool pass = false;
+            var obj = new TestObject();
+            obj.PropertyChanged += (sender, e) => { if (e.PropertyName == nameof(TestObject.AnyTypeProperty)) pass = true; };
+            obj.AnyTypeProperty = new object();
+            if (!pass) Assert.Fail($"Event not raised for property {nameof(TestObject.AnyTypeProperty)}");
+        }
+
+        [TestMethod]
+        public void DependantPropertyTest()
+        {
+            bool masterPass = false, slavePass = false;
+            var obj = new TestObject();
+            obj.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(TestObject.MagicNumber)) masterPass = true;
+                if (e.PropertyName == nameof(TestObject.DependantProperty)) slavePass = true;
+            };
+            obj.MagicNumber = 1500.2900;
+            if (!masterPass) Assert.Fail($"Event not raised for property {nameof(TestObject.MagicNumber)}");
+            if (!slavePass) Assert.Fail($"Event not raised for property {nameof(TestObject.DependantProperty)}");
+        }
+
+        [TestMethod]
+        public void CollectionChangedTest()
+        {
+            bool pass = false;
+            var obj = new TestObject();            
+            obj.PropertyChanged += (sender, e) => { if (e.PropertyName == nameof(TestObject.Collection)) pass = true; };
+            obj.Collection.Add(new object());
+            if (!pass) Assert.Fail($"Event not raised for property {nameof(TestObject.Collection)}");
+        }
+
+        [TestMethod]
+        public void CollectionItemChangedTest()
+        {
+            bool pass = false;
+            var obj = new TestObject();
+            var sub = new TestObject();           
+            obj.Collection.Add(sub);
+            obj.PropertyChanged += (sender, e) => { if (e.PropertyName == nameof(TestObject.Collection)) pass = true; };
+            sub.AnyTypeProperty = new object();
+            if (!pass) Assert.Fail($"Event not raised for property {nameof(TestObject.Collection)}");
+        }
+
+    }
+}
---
+++ Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85801f12-d995-4359-b078-6da7a010c652")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
