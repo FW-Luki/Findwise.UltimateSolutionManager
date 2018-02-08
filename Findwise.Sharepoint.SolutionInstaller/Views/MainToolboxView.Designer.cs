d31a0f53f32446a11d913fcbb1718f19f9bf719e
Lukasz Woojcik <lukasz.wojcik@findwise.com>
2018-02-07 16:12:44 -0800

    Toolbox Categories support.

--- Findwise.Sharepoint.SolutionInstaller/Views/MainToolboxView.Designer.cs
+++ Findwise.Sharepoint.SolutionInstaller/Views/MainToolboxView.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sortButton = new Findwise.Sharepoint.SolutionInstaller.Controls.NoFocusButton();
             this.ToolboxPanel = new Findwise.Sharepoint.SolutionInstaller.Controls.SizeablePanel();
             this.SuspendLayout();
+            // 
+            // sortButton
+            // 
+            this.sortButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sortButton.Location = new System.Drawing.Point(115, -1);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(21, 21);
+            this.sortButton.TabIndex = 6;
+            this.sortButton.UseVisualStyleBackColor = true;
             // 
             // ToolboxPanel
             // 
             this.ToolboxPanel.Caption = "Toolbox";
+            this.ToolboxPanel.CollapseButtonCaption = "Collapse";
+            this.ToolboxPanel.Collapsed = false;
             this.ToolboxPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ToolboxPanel.ExpandButtonCaption = "Expand";
             this.ToolboxPanel.GripPosition = System.Windows.Forms.DockStyle.Right;
             this.ToolboxPanel.Location = new System.Drawing.Point(0, 0);
             this.ToolboxPanel.Name = "ToolboxPanel";
@@ -45,6 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.sortButton);
             this.Controls.Add(this.ToolboxPanel);
             this.Name = "MainToolboxViewDesigner";
             this.Size = new System.Drawing.Size(160, 150);
@@ -55,5 +69,6 @@
         #endregion
 
         private Controls.SizeablePanel ToolboxPanel;
+        private Controls.NoFocusButton sortButton;
     }
 }
