5f83d89f8de7e18b79285ddb4ce150aca9b376ea
Łukasz Wójcik <lukasz.wojcik@findwise.com>
2018-02-28 03:25:43 +0100

    Added Configuration project as submodule.

--- InstallerModules/ImpactRulesCreator/Configuration.cs
+++ InstallerModules/ImpactRulesCreator/Configuration.cs
@@ -21,12 +21,12 @@ namespace ImpactRulesCreator
         [DisplayName("Site")]
         public string SpSite { get; set; }
 
-        [Description("Simultaneous requests - Request up to the specified number of documents at a time and do not wait between requests\nDelay between requests - Request one document at a time and wait the specified time between requests")]
+        [Description("Simultaneous requests - Request up to the specified number of documents at a time and do not wait between requests\r\nDelay between requests - Request one document at a time and wait the specified time between requests")]
         [DisplayName("Behavior")]
         public SiteHitRuleBehavior Behavior { get; set; }
 
         [DefaultValue(1)]
-        [Description("Indicate how the crawler will request documents from this site.\n\nValue to use for maximum requests or seconds of delay, according to behavior.\n\nIf a value is SimultaneousRequests, the hit rate is the maximum number of simultaneous requests.\nIf a value is DelayBetweenRequests, then hit rate is the number of seconds to delay between requests to the server.")]
+        [Description("Indicate how the crawler will request documents from this site.\r\nValue to use for maximum requests or seconds of delay, according to behavior.\r\nIf a value is SimultaneousRequests, the hit rate is the maximum number of simultaneous requests.\nIf a value is DelayBetweenRequests, then hit rate is the number of seconds to delay between requests to the server.")]
         [DisplayName("Hit Rate")]
         public int HitRate { get; set; }
     }
